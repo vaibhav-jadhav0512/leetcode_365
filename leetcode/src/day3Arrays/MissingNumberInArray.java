b46244d6136e9519c8eeb91c67262610400103e3
Vaibhav Jadhav <vjadhav@microstrategy.com>
2024-01-05 17:55:39 +0530

    MissingNumberInArray

---
+++ leetcode/src/day3Arrays/MissingNumberInArray.java
@@ -0,0 +1,31 @@
+package day3Arrays;
+
+public class MissingNumberInArray {
+	public static void main(String[] args) {
+		int[] arr = new int[] {9,6,4,2,3,5,7,0,1};
+		System.out.println(missingNumberInArray(arr));
+	}
+
+	private static int missingNumberInArray(int[] arr) {
+		int sum=(arr.length*(arr.length+1))/2;
+		int diff=0;
+		for(int i=0;i<arr.length;i++) {
+			diff+=arr[i];
+		}
+		return sum-diff;
+	}
+}
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+//Example 1:
+//Input: nums = [3,0,1]
+//Output: 2
+//Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+//Example 2:
+//Input: nums = [0,1]
+//Output: 2
+//Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+//Example 3:
+//Input: nums = [9,6,4,2,3,5,7,0,1]
+//Output: 8
+//Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
